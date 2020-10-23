d7d570933a6297b3060708c418f917e4cc88bc6a
Ralf S. Engelschall <rse@engelschall.com>
2020-10-24 01:07:40 +0200

    fix progress handling

--- update-helper-api.js
+++ update-helper-api.js
@@ -60,11 +60,11 @@ class UpdateHelper {
     }
 
     /*  perform the upgrade operation  */
-    async upgrade () {
+    async update () {
         /*  download ZIP archive of CLI binary  */
         const url = "https://github.com/rse/update-helper/releases/download/" +
             `${pkg.version}/update-helper-cli-${this.sys}-x64.zip`
-        this.progress("downloading ZIP archive of CLI binary", 0.0)
+        this.options.progress("downloading ZIP archive of CLI binary", 0.0)
         const req = got({
             method:       "GET",
             url:          url,
@@ -75,22 +75,22 @@ class UpdateHelper {
             let completed = p.transferred / p.total
             if (isNaN(completed))
                 completed = 0
-            this.progress("downloading ZIP archive of CLI binary", completed)
+            this.options.progress("downloading ZIP archive of CLI binary", completed)
         })
         const response = await req
         const tmpfile = tmp.fileSync()
         await fs.promises.writeFile(tmpfile.name, response.body, { encoding: null })
-        this.progress("downloading ZIP archive of CLI binary", 1.0)
+        this.options.progress("downloading ZIP archive of CLI binary", 1.0)
 
         /*  extract ZIP archive of CLI binary  */
-        this.progress("extracting ZIP archive of CLI binary", 0.0)
+        this.options.progress("extracting ZIP archive of CLI binary", 0.0)
         const tmpdir = tmp.dirSync()
         const zip = new AdmZip(tmpfile.name)
         const dirCreated = {}
         const entries = zip.getEntries()
         for (let i = 0; i < entries.length; i++) {
             const entry = entries[i]
-            this.progress("extracting ZIP archive of CLI binary", i / entries.length)
+            this.options.progress("extracting ZIP archive of CLI binary", i / entries.length)
 
             /*  determine result file path on filesystem  */
             const filePath = path.join(tmpdir.name, entry.entryName)
@@ -117,7 +117,7 @@ class UpdateHelper {
                 await fs.promises.chmod(filePath, (entry.attr >> 16) & 0x1ff)
             }
         }
-        this.progress("extracting ZIP archive of CLI binary", 1.0)
+        this.options.progress("extracting ZIP archive of CLI binary", 1.0)
         tmpfile.removeCallback()
 
         /*  final sanity check  */
@@ -145,6 +145,7 @@ class UpdateHelper {
             args = args.concat([ "--cleanup", this.options.cleanup ])
         if (this.options.execute !== "")
             args = args.concat([ "--execute", this.options.execute ])
+        this.options.progress("executing CLI binary", 0.0)
         await execa(cli, args, {
             env: {
                 UPDATE_HELPER_CLEANUP_DIR: tmpdir.name
