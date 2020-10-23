2787971adfd376303e03e4f823c9497c63bc1623
Ralf S. Engelschall <rse@engelschall.com>
2020-10-24 00:38:35 +0200

    initially place under version control

---
+++ update-helper-api.js
@@ -0,0 +1,188 @@
+/*
+**  update-helper -- Application Update Process Helper Utility
+**  Copyright (c) 2020 Dr. Ralf S. Engelschall <rse@engelschall.com>
+**
+**  Permission is hereby granted, free of charge, to any person obtaining
+**  a copy of this software and associated documentation files (the
+**  "Software"), to deal in the Software without restriction, including
+**  without limitation the rights to use, copy, modify, merge, publish,
+**  distribute, sublicense, and/or sell copies of the Software, and to
+**  permit persons to whom the Software is furnished to do so, subject to
+**  the following conditions:
+**
+**  The above copyright notice and this permission notice shall be included
+**  in all copies or substantial portions of the Software.
+**
+**  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+**  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+**  MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+**  IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+**  CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+**  TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+**  SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+const fs     = require("fs")
+const os     = require("os")
+const path   = require("path")
+const got    = require("got")
+const execa  = require("execa")
+const AdmZip = require("adm-zip")
+const tmp    = require("tmp")
+const mkdirp = require("mkdirp")
+const pkg    = require("./package.json")
+
+class UpdateHelper {
+    constructor (options = {}) {
+        /*  provide option defaults  */
+        this.options = Object.assign({}, {
+            kill:     0,
+            wait:     0,
+            source:   "",
+            target:   "",
+            cleanup:  [],
+            execute:  "",
+            progress: (step, percent) => {}
+        }, options)
+
+        /*  sanity check usage  */
+        if (this.options.source === "")
+            throw new Error("mandatory option 'source' missing")
+        if (this.options.target === "")
+            throw new Error("mandatory option 'target' missing")
+
+        /*  determine current system  */
+        this.sys = ""
+        if      (os.platform() === "win32")  this.sys = "win"
+        else if (os.platform() === "darwin") this.sys = "mac"
+        else if (os.platform() === "linux")  this.sys = "lnx"
+        else throw new Error(`current platform "${os.platform()}" is not supported`)
+    }
+
+    /*  perform the upgrade operation  */
+    async upgrade () {
+        /*  download ZIP archive of CLI binary  */
+        const url = "https://github.com/rse/update-helper/releases/download/" +
+            `${pkg.version}/update-helper-cli-${this.sys}-x64.zip`
+        this.progress("downloading ZIP archive of CLI binary", 0.0)
+        const req = got({
+            method:       "GET",
+            url:          url,
+            headers:      { "User-Agent": `${pkg.name}/${pkg.version}` },
+            responseType: "buffer"
+        })
+        req.on("downloadProgress", (p) => {
+            let completed = p.transferred / p.total
+            if (isNaN(completed))
+                completed = 0
+            this.progress("downloading ZIP archive of CLI binary", completed)
+        })
+        const response = await req
+        const tmpfile = tmp.fileSync()
+        await fs.promises.writeFile(tmpfile.name, response.body, { encoding: null })
+        this.progress("downloading ZIP archive of CLI binary", 1.0)
+
+        /*  extract ZIP archive of CLI binary  */
+        this.progress("extracting ZIP archive of CLI binary", 0.0)
+        const tmpdir = tmp.dirSync()
+        const zip = new AdmZip(tmpfile.name)
+        const dirCreated = {}
+        const entries = zip.getEntries()
+        for (let i = 0; i < entries.length; i++) {
+            const entry = entries[i]
+            this.progress("extracting ZIP archive of CLI binary", i / entries.length)
+
+            /*  determine result file path on filesystem  */
+            const filePath = path.join(tmpdir.name, entry.entryName)
+
+            /*  determine directory path and automatically create missing directories  */
+            const dirPath = entry.isDirectory ? filePath : path.dirname(filePath)
+            if (!dirCreated[dirPath]) {
+                await mkdirp(dirPath)
+                dirCreated[dirPath] = true
+            }
+
+            /*  create resulting entry  */
+            if (((entry.attr >> 28) & 0x0F) === 10) {
+                /*  case 1: symbolic link  */
+                const target = zip.readFile(entry).toString()
+                await fs.promises.symlink(target, filePath)
+                if (os.platform() === "darwin")
+                    await fs.promises.lchmod(filePath, (entry.attr >> 16) & 0x1ff)
+            }
+            else if (!entry.isDirectory) {
+                /*  case 2: regular file  */
+                const data = zip.readFile(entry)
+                await fs.promises.writeFile(filePath, data, { encoding: null })
+                await fs.promises.chmod(filePath, (entry.attr >> 16) & 0x1ff)
+            }
+        }
+        this.progress("extracting ZIP archive of CLI binary", 1.0)
+        tmpfile.removeCallback()
+
+        /*  final sanity check  */
+        let cli
+        if (this.sys === "win")
+            cli = path.join(tmpdir.name, "update-helper-cli-win-x64.exe")
+        else if (this.sys === "mac")
+            cli = path.join(tmpdir.name, "update-helper-cli-mac-x64")
+        else if (this.sys === "lnx")
+            cli = path.join(tmpdir.name, "update-helper-cli-lnx-x64")
+        const accessible = await fs.promises.access(cli, fs.constants.F_OK | fs.constants.R_OK)
+            .then(() => true).catch(() => false)
+        if (!accessible)
+            throw new Error("cannot find CLI binary in downloaded content")
+
+        /*  pass-through execution to CLI  */
+        let args = []
+        if (this.options.kill > 0)
+            args = args.concat([ "--kill", this.options.kill ])
+        if (this.options.wait > 0)
+            args = args.concat([ "--wait", this.options.wait ])
+        args = args.concat([ "--source", this.options.source ])
+        args = args.concat([ "--target", this.options.target ])
+        if (this.options.cleanup !== "")
+            args = args.concat([ "--cleanup", this.options.cleanup ])
+        if (this.options.execute !== "")
+            args = args.concat([ "--execute", this.options.execute ])
+        await execa(cli, args, {
+            env: {
+                UPDATE_HELPER_CLEANUP_DIR: tmpdir.name
+            }
+        })
+    }
+
+    /*  perform the cleanup after upgrading  */
+    async cleanup () {
+        const cleanup = this.process.env.UPDATE_HELPER_CLEANUP_DIR
+        if (typeof cleanup === "string" && cleanup !== "") {
+            /*  cleanup environment  */
+            delete this.process.env.UPDATE_HELPER_CLEANUP_DIR
+
+            /*  final sanity check  */
+            let cli
+            if (this.sys === "win")
+                cli = path.join(cleanup, "update-helper-cli-win-x64.exe")
+            else if (this.sys === "mac")
+                cli = path.join(cleanup, "update-helper-cli-mac-x64")
+            else if (this.sys === "lnx")
+                cli = path.join(cleanup, "update-helper-cli-lnx-x64")
+            const accessible = await fs.promises.access(cli, fs.constants.F_OK | fs.constants.R_OK)
+                .then(() => true).catch(() => false)
+            if (!accessible)
+                throw new Error("cannot find CLI binary in cleanup directory")
+
+            /*  perform cleanup  */
+            try {
+                await fs.promises.unlink(cli)
+                await fs.promises.unlink(cleanup)
+            }
+            catch (ex) {
+                /*  NOP  */
+            }
+        }
+    }
+}
+
+module.exports = UpdateHelper
+
---
+++ update-helper-cli.js
@@ -0,0 +1,143 @@
+#!/usr/bin/env node
+/*!
+**  update-helper -- Application Update Process Helper Utility
+**  Copyright (c) 2020 Dr. Ralf S. Engelschall <rse@engelschall.com>
+**
+**  Permission is hereby granted, free of charge, to any person obtaining
+**  a copy of this software and associated documentation files (the
+**  "Software"), to deal in the Software without restriction, including
+**  without limitation the rights to use, copy, modify, merge, publish,
+**  distribute, sublicense, and/or sell copies of the Software, and to
+**  permit persons to whom the Software is furnished to do so, subject to
+**  the following conditions:
+**
+**  The above copyright notice and this permission notice shall be included
+**  in all copies or substantial portions of the Software.
+**
+**  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+**  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+**  MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+**  IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+**  CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+**  TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+**  SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+/*  external requirements  */
+const yargs   = require("yargs")
+const execa   = require("execa")
+const copy    = require("@danieldietrich/copy")
+const rimraf  = require("rimraf")
+
+;(async () => {
+    /*  parse command-line options  */
+    const usage =
+        "Usage: update-helper-cli" +
+        " [-k|--kill <pid>]" +
+        " [-w|--wait <ms>]" +
+        " [-s|--source <file>|<directory>]" +
+        " [-t|--target <file>|<directory>]" +
+        " [-c|--cleanup <file>|<directory>]" +
+        " [-e|--execute <file>]"
+    const opts = yargs()
+        .parserConfiguration({
+            "set-placeholder-key": true,
+            "halt-at-non-option":  true
+        })
+        .usage(usage)
+        .option("k", {
+            alias:    "kill",
+            type:     "number",
+            describe: "process id to kill",
+            default:  0
+        })
+        .option("w", {
+            alias:    "wait",
+            type:     "number",
+            describe: "milliseconds to wait after kill and before copying source to target",
+            default:  0
+        })
+        .option("s", {
+            alias:    "source",
+            type:     "string",
+            describe: "source file or directory",
+            default:  ""
+        })
+        .option("t", {
+            alias:    "target",
+            type:     "string",
+            describe: "target file or directory",
+            default:  ""
+        })
+        .option("c", {
+            alias:    "cleanup",
+            type:     "array",
+            describe: "cleanup file or directory after update",
+            default:  []
+        })
+        .option("e", {
+            alias:    "execute",
+            type:     "string",
+            describe: "execute program after update",
+            default:  ""
+        })
+        .version(false)
+        .help(true)
+        .showHelpOnFail(true)
+        .strict(true)
+        .parse(process.argv.slice(2))
+    if (opts._.length !== 0) {
+        process.stderr.write(`${usage}\n`)
+        process.exit(1)
+    }
+    if (typeof opts.cleanup === "string")
+        opts.cleanup = [ opts.cleanup ]
+    if (opts.source === "")
+        throw new Error("mandatory source file or directory missing")
+    if (opts.target === "")
+        throw new Error("mandatory target file or directory missing")
+
+    /*  optionally kill old program  */
+    if (opts.kill > 0)
+        process.kill(opts.kill, "SIGTERM")
+
+    /*  optionally wait
+        (which usually gives calling application a chance to
+        terminate in order to avoid busy files)  */
+    if (opts.wait > 0)
+        await new Promise((resolve) => setTimeout(resolve, opts.wait))
+
+    /*  copy source to target
+        (which usually means to replace the calling application executable)  */
+    await copy(opts.source, opts.target, {
+        overwrite: true
+    })
+
+    /*  optionally cleanup resources
+        (which usually means to remove the source)  */
+    for (const resource of opts.cleanup) {
+        await new Promise((resolve) => {
+            rimraf(resource, { disableGlob: true }, resolve)
+        })
+    }
+
+    /*  optionally execute program
+        (which usually restarts the calling application again)  */
+    if (opts.execute !== "") {
+        const proc = execa.command(opts.execute, {
+            stdio:    [ "ignore", "ignore", "ignore" ],
+            detached: true,
+            shell:    true
+        })
+        proc.unref()
+        await new Promise((resolve) => setTimeout(resolve, 1000))
+    }
+
+    /*  gracefully terminate  */
+    process.exit(0)
+})().catch((err) => {
+    /*  fatal error  */
+    process.stderr.write(`update-helper-cli: ERROR: ${err}\n`)
+    process.exit(1)
+})
+
